570aa2704b40e2b3b179c7840ee0a8cfdcf46118
FranLuque <luquefran97@hotmail.com>
2022-05-29 19:36:17 +0200

    StudentService.java modified

--- src/main/java/es/eoi/jdbc/service/StudentService.java
+++ src/main/java/es/eoi/jdbc/service/StudentService.java
@@ -1,4 +1,34 @@
 package es.eoi.jdbc.service;
 
+import es.eoi.jdbc.entity.Student;
+import es.eoi.jdbc.repository.StudentRepository;
+
+import java.util.List;
+
 public class StudentService {
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+    public Student findById(String id){
+
+        return null;
+    }
+    public List<Student> findAll(){
+
+        return null;
+    }
+    public Student create(Student student){
+
+        return student;
+    }
+    public boolean delete(String id){
+
+        return false;
+    }
+    public Student update(Student student){
+
+        return student;
+    }
 }
